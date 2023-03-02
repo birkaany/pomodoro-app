267c405e0d2b0f66283aecd8994b55511def78fd
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2023-03-03 02:04:10 +0300

    feat: auto stages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Labels from "./components/Labels";
 import Timer from "./components/Timer";
 
 function App() {
   const [selectedControl, setSelectedControl] = useState(0);
+
   return (
     <>
       <Labels
@@ -12,7 +13,10 @@ function App() {
           setSelectedControl(e);
         }}
       />
-      <Timer selected={selectedControl} />
+      <Timer
+        selected={selectedControl}
+        setSelectedControl={setSelectedControl}
+      />
     </>
   );
 }
