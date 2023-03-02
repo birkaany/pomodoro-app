63766e006e02c521b99062ec73e9221d9eff0379
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2023-03-03 02:03:53 +0300

    feat: auto stages

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,42 +2,43 @@ import React, { useState, useEffect } from "react";
 import ControlButton from "./ControlButton";
 
 const Timer = ({ selected }) => {
+  const stages = ["pomodoroTime", "shortBreakTime", "longBreakTime"];
+
   const [pomodoro, setPomodoro] = useState({
-    pomodoroTime: 25 * 60,
+    pomodoroTime: 1 * 2,
     shortBreakTime: 5 * 60,
     longBreakTime: 10 * 60,
-    isPaused: false,
+    isPaused: true,
     period: 4,
+    currentStage: stages[selected],
   });
-
-  const controllerValues = ["pomodoroTime", "shortBreakTime", "longBreakTime"];
   useEffect(() => {
     let timer = null;
+
     if (!pomodoro.isPaused) {
       timer = setInterval(() => {
         setPomodoro((prevPomodoro) => {
-          if (prevPomodoro[`${controllerValues[selected]}`] === 0) {
+          if (prevPomodoro[pomodoro.currentStage] === 0) {
+            prevPomodoro.currentStage = stages[selected];
             clearInterval(timer);
             return prevPomodoro;
           }
-          const returnValue = {
+          return {
             ...prevPomodoro,
-            [`${controllerValues[selected]}`]: prevPomodoro[`${controllerValues[selected]}`] - 1,
+            [pomodoro.currentStage]: prevPomodoro[pomodoro.currentStage] - 1,
           };
-
-          return returnValue;
         });
       }, 1000);
     }
 
     return () => clearInterval(timer);
-  }, [pomodoro.isPaused]);
+  }, [pomodoro.isPaused, pomodoro.currentStage]);
 
   function pauseTimer() {
-    setPomodoro((prevState) => {
+    setPomodoro((prevPomodoro) => {
       return {
-        ...prevState,
-        isPaused: !prevState.isPaused,
+        ...prevPomodoro,
+        isPaused: !prevPomodoro.isPaused,
       };
     });
   }
@@ -47,8 +48,8 @@ const Timer = ({ selected }) => {
       <div className="tw-timer-container">
         <div className="tw-timer">
           <div className="flex flex-col justify-center items-center font-semibold">
-            {Math.floor(pomodoro[`${controllerValues[selected]}`] / 60)}:{pomodoro[`${controllerValues[selected]}`] % 60 < 10 ? "0" : ""}
-            {pomodoro[`${controllerValues[selected]}`] % 60}
+            {Math.floor(pomodoro[pomodoro.currentStage] / 60)}:{pomodoro[pomodoro.currentStage] % 60 < 10 ? "0" : ""}
+            {pomodoro[pomodoro.currentStage] % 60}
             <ControlButton
               handleEvent={pauseTimer}
               isPaused={pomodoro.isPaused}
